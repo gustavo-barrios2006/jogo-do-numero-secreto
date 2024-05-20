bcbb5883657e7f34868f800f3d0b7dcfad0136f3
gustavo-barrios2006 <gabarrios2006@gmail.com>
2024-05-20 16:53:07 -0300

    Publicação inicial do projeto

---
+++ app.js
@@ -0,0 +1,62 @@
+function exibirMensagemInicial()
+{
+exibirTextoNaTela("h1", "Jogo de adivinhação");
+exibirTextoNaTela("p", "Digite um nï¿½mero de 0 a 10");
+}
+let listaDeNumerosSorteados=[];
+exibirMensagemInicial();
+let tentativas=1;
+function exibirTextoNaTela(tag, texto)
+{
+let campo=document.querySelector(tag);
+tag.innerHTML=texto;
+responsiveVoice.speak(texto, "Brazilian Portuguese Female", {rate:1.2});
+}
+let numeroSecreto=gerarNumeroAleatorio();
+function verificarChute()
+{
+let chute=document.querySelector("input").value;
+if(chute==numeroSecreto)
+{
+exibirTextoNaTela("h1", "Acertou!");
+let mensagemTentativas="Você acertou o número secreto com ${tentativas} ${tentativas==1?"tentativa":"tentativas"}`);
+exibirNumeroNaTela("p", mensagemTentativas);
+document.getElementById("reiniciar").removeAttribute("disable");
+}
+else
+if(chute>numeroSecreto)
+{
+exibirTextoNaTela("p", "O número secreto é maior");
+limparCampo();
+}
+else
+{
+exibirTextoNaTela("p", "O número secreto é menor");
+limparCampo();
+}
+tentativas++;
+}
+function gerarNumeroAleatorio()
+{
+let  numeroEscolhido=parseInt(Math.random()*10)+1;
+if(listaDeNumerosSorteados.includes(numeroEscolhido))
+{
+return gerarNumeroAleatorio();
+}
+else
+{
+listaDeNumerosSorteados.push(numeroEscolhido);
+return numeroEscolhido;
+}
+}
+function limparCampo()
+{
+document.querySelector("input").value="";
+}
+function reiniciarJogo()
+{
+tentativas=1;
+limparCampo();
+exibirMensagemInicial();
+document.getElementById("reiniciar").setAttribute("disabled", true);
+}
